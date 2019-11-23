81e3ca083b2918ada7bcdfcfc87d48423d929c3a
onyinye91 <onyinyeiloanugo@gmail.com>
2019-11-23 15:03:35 +0100

    delete and create a new file

--- weather.js
+++ weather.js
@@ -7,7 +7,6 @@ const geo = async (cityName) => {
     const response = await fetch(APIWeatherCall);
     const data = await response.json();
 
-    return(data);
 
 };
 
@@ -17,27 +16,27 @@ const geo = async (cityName) => {
 const cityForm = document.querySelector('form');
 const search = document.querySelector('.search');
 const details = document.querySelector('.details');
-const weather = document.querySelector('.weather_report')
 
 const updateUI = (data) => {
 
     const cityWeather = data.cityWeather;
-
+    
     console.log(cityWeather.weather[0].description);
 
     //update details template
-    details.innerHTML = `
-        <h5><strong>${cityWeather.name}</strong></h4>
-        <div class="timezone">${cityWeather.timezone}</div>    
-        <div class="windSpeed">${cityWeather.wind.speed}</div><br>
-        <div class="humidity">${cityWeather.main.humidity}</div>
-        <div class="weather">weather condition</div>          
-        <div class="tempC"><span>${cityWeather.main.temp}<span>
-            <span>F<span>
-        </div>
-    `;
+    // details.innerHTML = `
+    //     <h5><strong>${cityWeather.name}</strong></h4>
+    //     <div class="timezone">${cityWeather.timezone}</div>    
+    //     <div class="windSpeed">${cityWeather.wind.speed}</div><br>
+    //     <div class="humidity">${cityWeather.main.humidity}</div>
+    //     <div class="weather">weather condition</div>          
+    //     <div class="tempC"><span>${cityWeather.main.temp}<span>
+    //         <span>F<span>
+    //     </div>
+    // `;
 
-    //weather.textContent = `${cityWeather.name} weather condition has ${cityWeather.weather[0].description} a wind speed of ${cityWeather.wind.speed}, with humidity ${cityWeather.main.humidity} and temperature of ${cityWeather.main.temp} F  `
+    //weather.textContent = `
+       // ${cityWeather.name} weather condition has ${cityWeather.weather[0].description} a wind speed of ${cityWeather.wind.speed}, with humidity ${cityWeather.main.humidity} and temperature of ${cityWeather.main.temp} F`
 
 };    
 
@@ -48,7 +47,7 @@ const updateCity = async (city) => {
     //console.log(city);
     const cityWeather = await geo(city);
 
-    return{cityWeather};
+    return{cityWeather : cityWeather};
     
 };
 
@@ -64,7 +63,7 @@ cityForm.addEventListener('submit', e => {
     updateCity(city)
         //.then(data => console.log(data))
         .then(data => updateUI(data))
-        .catch(err => updateUI(err));
-    window.open('weather.html');
+        .catch(err => console.log(err));
+    //window.open('weather.html');
 
 });
