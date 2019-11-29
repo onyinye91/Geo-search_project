5189dc85b019656b6f4ccf3cab598d5db73d64ba
onyinye91 <onyinyeiloanugo@gmail.com>
2019-11-29 15:44:50 +0100

    configure eslint

--- apiCalls.js
+++ apiCalls.js
@@ -1,6 +1,6 @@
 const geo = async (cityName) => {
 
-    const api = "6e99790fa8ea80d4678d1444ccdf1e1c";
+    //const api = "6e99790fa8ea80d4678d1444ccdf1e1c";
     const APIWeatherCall = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName +'&APPID=6e99790fa8ea80d4678d1444ccdf1e1c'; 
    //const APIMapCall = 'https://www.mapquestapi.com/staticmap/v5/map?key=7xJu4S9lffITaZdfAxSZkPPwunOEXcwl&center=' + cityName +',MA&size=@2x';
 
@@ -18,12 +18,13 @@ const geo = async (cityName) => {
 
 
 const cityForm = document.querySelector('form');
-const search = document.querySelector('.search');
+//const search = document.querySelector('.search');
 //const details = document.querySelector('.details');
 const weather = document.querySelector('.details');
-const button = document.querySelector('.tempF');
+//const button = document.querySelector('.tempF');
 const city = cityForm.myCity.value.trim();
 const map = document.querySelector('#map');
+
 
 // console.log(button.classList);
 
@@ -31,9 +32,12 @@ const updateUI = (data) => {
 
     const cityWeather = data.cityWeather;
 
+    const lon = cityWeather.coord.lon;
+    const lat = cityWeather.coord.lat;
     console.log(cityWeather.weather[0].description);
     console.log(cityWeather.coord.lat);
     console.log(cityWeather.coord.lon);
+    
 
     //update details template
     weather.innerHTML = `
@@ -52,7 +56,90 @@ const updateUI = (data) => {
         </div>
         <iframe src="https://www.facebook.com/plugins/share_button.php?href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&layout=button_count&size=small&width=96&height=20&appId" width="96" height="20" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
         `;
+
+    //     // The ymaps.ready() function will be called when
+    // // all the API components are loaded and the DOM tree is generated.
+    // ymaps.ready(init);
+    // function init(){ 
+    //     // Creating the map.    
+    //     var myMap = new ymaps.Map("map", {
+    //         // The map center coordinates.
+    //         // Default order: “latitude, longitude”.
+    //         // To not manually determine the map center coordinates,
+    //         // use the Coordinate detection tool.
+    //         center: [lat,lon],
+    //         // Zoom level. Acceptable values:
+    //         // from 0 (the entire world) to 19.
+    //         zoom: 7
+    //     });
+        
+
+    // }  
     
+    //MAP/////////
+    ymaps.ready(function () {
+        var myMap = new ymaps.Map('map', {
+                center: [lat, lon],
+                zoom: 9
+            }, {
+                searchControlProvider: 'yandex#search'
+            }),
+    
+            // Creating a content layout.
+            MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+                '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+            ),
+    
+            myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+                hintContent: 'A custom placemark icon',
+                balloonContent: 'This is a pretty placemark'
+            }, {
+                /**
+                 * Options.
+                 * You must specify this type of layout.
+                 */
+                iconLayout: 'default#image',
+                // Custom image for the placemark icon.
+                iconImageHref: 'images/myIcon.gif',
+                // The size of the placemark.
+                iconImageSize: [30, 42],
+                /**
+                 * The offset of the upper left corner of the icon relative
+                 * to its "tail" (the anchor point).
+                 */
+                iconImageOffset: [-5, -38]
+
+            }),
+    
+            myPlacemarkWithContent = new ymaps.Placemark([55.661574, 37.573856], {
+                hintContent: 'A custom placemark icon with contents',
+                balloonContent: 'This one — for Christmas',
+                iconContent: '12'
+            }, {
+                /**
+                 * Options.
+                 * You must specify this type of layout.
+                 */
+                iconLayout: 'default#imageWithContent',
+                // Custom image for the placemark icon.
+                iconImageHref: 'images/ball.png',
+                // The size of the placemark.
+                iconImageSize: [48, 48],
+                /**
+                 * The offset of the upper left corner of the icon relative
+                 * to its "tail" (the anchor point).
+                 */
+                iconImageOffset: [-24, -24],
+                // Offset of the layer with content relative to the layer with the image.
+                iconContentOffset: [15, 15],
+                // Content layout.
+                iconContentLayout: MyIconContentLayout
+            });
+    
+        myMap.geoObjects
+            .add(myPlacemark)
+            .add(myPlacemarkWithContent);
+    });   
 }; 
 
 const showError = (err) => {
@@ -103,6 +190,8 @@ cityForm.addEventListener('submit', e => {
         .catch(err => showError(err.message))
     //window.open('weather.html');
     document.getElementById("map").style.height = "250px";
+    document.getElementById("map").style.width = "100%";
+
 
 });
 //conversion from k to c
@@ -134,86 +223,4 @@ const timeZone = (secs) => {
 
 //console.log(timeZone(3600));
 
-// The ymaps.ready() function will be called when
-    // all the API components are loaded and the DOM tree is generated.
-    ymaps.ready(init);
-    function init(){ 
-        // Creating the map.    
-        var myMap = new ymaps.Map("map", {
-            // The map center coordinates.
-            // Default order: “latitude, longitude”.
-            // To not manually determine the map center coordinates,
-            // use the Coordinate detection tool.
-            center: [6.46,3.39],
-            // Zoom level. Acceptable values:
-            // from 0 (the entire world) to 19.
-            zoom: 7
-        });
-    }
-
-    // ymaps.ready(function () {
-    //     var myMap = new ymaps.Map('map', {
-    //             center: [55.751574, 37.573856],
-    //             zoom: 9
-    //         }, {
-    //             searchControlProvider: 'yandex#search'
-    //         }),
-    
-    //         // Creating a content layout.
-    //         MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-    //             '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
-    //         ),
-    
-    //         myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-    //             hintContent: 'A custom placemark icon',
-    //             balloonContent: 'This is a pretty placemark'
-    //         }, {
-    //             /**
-    //              * Options.
-    //              * You must specify this type of layout.
-    //              */
-    //             iconLayout: 'default#image',
-    //             // Custom image for the placemark icon.
-    //             iconImageHref: 'images/myIcon.gif',
-    //             // The size of the placemark.
-    //             iconImageSize: [30, 42],
-    //             /**
-    //              * The offset of the upper left corner of the icon relative
-    //              * to its "tail" (the anchor point).
-    //              */
-    //             iconImageOffset: [-5, -38]
-    //         }),
-    
-    //         myPlacemarkWithContent = new ymaps.Placemark([55.661574, 37.573856], {
-    //             hintContent: 'A custom placemark icon with contents',
-    //             balloonContent: 'This one — for Christmas',
-    //             iconContent: '12'
-    //         }, {
-    //             /**
-    //              * Options.
-    //              * You must specify this type of layout.
-    //              */
-    //             iconLayout: 'default#imageWithContent',
-    //             // Custom image for the placemark icon.
-    //             iconImageHref: 'images/ball.png',
-    //             // The size of the placemark.
-    //             iconImageSize: [48, 48],
-    //             /**
-    //              * The offset of the upper left corner of the icon relative
-    //              * to its "tail" (the anchor point).
-    //              */
-    //             iconImageOffset: [-24, -24],
-    //             // Offset of the layer with content relative to the layer with the image.
-    //             iconContentOffset: [15, 15],
-    //             // Content layout.
-    //             iconContentLayout: MyIconContentLayout
-    //         });
-    
-    //     myMap.geoObjects
-    //         .add(myPlacemark)
-    //         .add(myPlacemarkWithContent);
-    // });
-    
-
-
-  
+   
